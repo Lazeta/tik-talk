7a5a6987f945f9b56454a3b6a9efba0a2d7e5b47
Chekh Stanislav <fkrjcjdjifkdgdjsjjfmknckdnc@gmail.com>
2025-07-27 05:16:11 +0300

    feat: added routing, bg logo, layout component, profile-card component, side-bar component, pipes img, login-page component, profile-page and search-page components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { ProfileCard } from "./common-ui/profile-card/profile-card";
-import { ProfileService } from './data/services/profile.service';
-import { Profile } from './data/interfaces/profile.interface';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ProfileCard, CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  profileService: ProfileService = inject(ProfileService);
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts()
-    .subscribe((data: Profile[]) => {
-      this.profiles = data;
-      console.log('Полученные данные:', this.profiles);
-    })
-  }
+   
 }
---
+++ src/app/pages/search-page/search-page.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Profile } from '../../data/interfaces/profile.interface';
+import { ProfileService } from '../../data/services/profile.service';
+import { ProfileCard } from "../../common-ui/profile-card/profile-card";
+
+@Component({
+  selector: 'app-search-page',
+  imports: [CommonModule, ProfileCard],
+  templateUrl: './search-page.html',
+  styleUrl: './search-page.scss'
+})
+export class SearchPage {
+  profileService: ProfileService = inject(ProfileService);
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts()
+    .subscribe((data: Profile[]) => {
+      this.profiles = data;
+      console.log('Полученные данные:', this.profiles);
+    })
+  }
+}
